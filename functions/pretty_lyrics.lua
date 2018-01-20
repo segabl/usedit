22cf973782d317b5f3600f628814e14e2033e1ec
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-20 13:28:11 +0100

    Change way spaces are replaced

--- functions/pretty_lyrics.lua
+++ functions/pretty_lyrics.lua
@@ -3,7 +3,7 @@ local next_note
 for _, track in pairs(song.tracks) do
   for i, note in ipairs(track) do
     note.lyrics = note.lyrics:gsub("[´`]", "'")
-    note.lyrics = note.lyrics:gsub("(%S)%s~", "%1~")
+    note.lyrics = note.lyrics:gsub("%s~", "~")
     note.lyrics = note.lyrics:gsub("~%s(%S)", "~%1")
     note.lyrics = note.lyrics:gsub("[.,; ]+\"", "\"")
     if note.type == NoteType.LINEBREAK then
