5a849c40abedac3fdb768a48f27d13ca4bf3f2a4
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-01-20 13:29:44 +0100

    Improve BPM multiplication
    Added multiplication to medley start and end beat, fixed notes getting length 0

--- functions/multiply_BPM.lua
+++ functions/multiply_BPM.lua
@@ -1,11 +1,13 @@
-local amount = 2 --the user should be prompted for this in the future
+local amount = 0.5 --the user should be prompted for this in the future
 
 song.bpm = song.bpm * amount
 for _, track in pairs(song.tracks) do
   for _, note in ipairs(track) do
-    note.position = note.position * amount
-    note.length = note.length * amount
+    note.position = math.floor(note.position * amount)
+    note.length = math.max(1, math.floor(note.length * amount))
   end
 end
+song.medley_start = math.floor(song.medley_start * amount)
+song.medley_end = math.floor(song.medley_end * amount)
 
 log(0, "Changed song BPM to " .. song.bpm)
