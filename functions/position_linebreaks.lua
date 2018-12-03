b55abe2aa92ef9163d49a59d03ee322552d35585
Sebastian Gabl <sebastian.gabl@gmx.at>
2018-12-03 12:19:03 +0100

    Add minimum of 1 spacing if possible

--- functions/position_linebreaks.lua
+++ functions/position_linebreaks.lua
@@ -1,17 +1,20 @@
 local floor = math.floor
+local max = math.max
 
-local amount = 0.25 --the user should be prompted for this in the future
+local amount = 0.334 --the user should be prompted for this in the future
 
 local changes = 0
 
-local next_note, prev_note, old_pos
+local next_note, prev_note, old_pos, len, tlen
 for _, track in pairs(song.tracks) do
   for i, note in ipairs(track) do
     if i > 1 and i < #track and note.type == NoteType.LINEBREAK then
       prev_note = track[i - 1]
       next_note = track[i + 1]
       old_pos = note.position
-      note.position = prev_note.position + prev_note.length + floor((next_note.position - prev_note.position - prev_note.length) * amount)
+      len = next_note.position - prev_note.position - prev_note.length
+      tlen = floor(len * amount)
+      note.position = prev_note.position + prev_note.length + (tlen > 0 and tlen or floor(len * 0.5))
       if old_pos ~= note.position then
         changes = changes + 1
       end
